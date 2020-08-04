8e92a186a8602624db2a269aef81c463223a8590
Gurinder Batth <44367969+GurinderB@users.noreply.github.com>
2020-08-04 08:37:49 -0700

    Revert "Dawn"

--- src/com/alienation/coregamefiles/gamefunctionclasses/AttackAlien.java
+++ src/com/alienation/coregamefiles/gamefunctionclasses/AttackAlien.java
@@ -1,7 +1,6 @@
 package com.alienation.coregamefiles.gamefunctionclasses;
 
 import com.alienation.coregamefiles.charactersetc.Alien;
-import com.alienation.coregamefiles.charactersetc.AlienObjects;
 import com.alienation.coregamefiles.enums.Rooms;
 import com.alienation.coregamefiles.enums.Weapons;
 import com.alienation.coregamefiles.gameart.Death;
@@ -19,7 +18,6 @@ import static com.alienation.coregamefiles.gamefunctionclasses.AlienAttack.alien
 import static com.alienation.coregamefiles.gamefunctionclasses.Menu.*;
 import static com.alienation.coregamefiles.hashmaps.AvailableItemsHashMap.getAvailableItemsMap;
 
-
 public class AttackAlien {
 
     /* -- AttackAlien the Alien in the room -- START */
@@ -33,8 +31,7 @@ public class AttackAlien {
 
         if(roomItems.contains(getItem2()) || roomItems.contains(getItem1())) {
             try {
-                //Optional<Alien> maybeAlien = Alien.getAlien(getItem1());
-                Optional<Alien> maybeAlien = AlienObjects.getAlien(getItem1());
+                Optional<Alien> maybeAlien = Alien.getAlien(getItem1());
 
                 if(maybeAlien.isPresent()){
                     Alien alien = maybeAlien.get();
